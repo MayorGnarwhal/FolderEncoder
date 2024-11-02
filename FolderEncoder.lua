6b0d894573de49568f78b5e7ab78586c6a1025d4
Harris Fort <46070329+MayorGnarwhal@users.noreply.github.com>
2024-11-02 18:47:31 -0400

    Enforces reserved keys used for encoding

--- FolderEncoder.lua
+++ FolderEncoder.lua
@@ -1,8 +1,22 @@
+--[[
+	Easily convert folders to tables and vice versa
+	Supports attributes, subfolders, and non-folder instance types
+		Instance properties are not encoded except for the Value of ValueBases
+]]
+
 local FolderEncoder = {}
+
+--// Constants
+local ATTRIBUTE_PREFIX = "__attr/" -- prefix of attribute encodings
+local RESERVED_DATA_KEYS = { -- keys used for encoding data. input data should not use these keys
+	"__value",
+	"__className",
+} 
+
 
 --// Helper functions
 local function GetAttributeName(key)
-	local attrStart, attrEnd = string.find(key, "attr_")
+	local attrStart, attrEnd = string.find(key, ATTRIBUTE_PREFIX)
 	if attrStart == 1 then
 		return string.sub(key, attrEnd + 1)
 	end
@@ -27,13 +41,13 @@ function FolderEncoder.GetValueBase(var: any)
 	return valueBase
 end
 
-function FolderEncoder.FolderToTable(dataFile: folder|ValueBase)
-	assert(typeof(dataFile) == "Instance" and (dataFile:IsA("Folder") or dataFile:IsA("ValueBase")), 
-		"Must provide Folder or ValueBase for serialization.")
+function FolderEncoder.FolderToTable(dataFile: Instance): {} | any
+	assert(typeof(dataFile) == "Instance", "Must provide an instance for serialization.")
 	
 	local data = {}
+	local dataFileChildren = dataFile:GetChildren()
 	
-	if dataFile:IsA("ValueBase") and #dataFile:GetChildren() == 0 then
+	if dataFile:IsA("ValueBase") and #dataFileChildren == 0 then
 		local attributes = dataFile:GetAttributes()
 		if not next(attributes) then
 			return dataFile.Value
@@ -43,40 +57,45 @@ function FolderEncoder.FolderToTable(dataFile: folder|ValueBase)
 	else
 		if dataFile:IsA("ValueBase") then
 			data.__value = dataFile.Value
+		elseif not dataFile:IsA("Folder") then
+			data.__className = dataFile.ClassName
 		end
 		
-		for i, entry in pairs(dataFile:GetChildren()) do
+		for i, entry in pairs(dataFileChildren) do
 			local key = tonumber(entry.Name) or entry.Name
+			if table.find(RESERVED_DATA_KEYS, key) then 
+				error(`Reserved data key is being used ({key})`)
+			elseif data[key] ~= nil then
+				error(`Folder encoding has duplicate key ({key} in {entry:GetFullName()})`)
+			end
 			data[key] = FolderEncoder.FolderToTable(entry)
 		end
 	end
 	
 	for attribute, value in pairs(dataFile:GetAttributes()) do
-		local key = "attr_" .. attribute
+		local key = ATTRIBUTE_PREFIX .. attribute
 		data[key] = value
 	end
 
 	return data
 end
 
-function FolderEncoder.TableToFolder(data: table|any, folderName: string?, folderParent: Instance?)
+function FolderEncoder.TableToFolder(data: {}|any, folderName: string?, folderParent: Instance?): Folder | ValueBase
 	local dataFile
 	
 	if typeof(data) == "table" then
-		if data.__value then
+		if data.__value ~= nil then
 			dataFile = FolderEncoder.GetValueBase(data.__value)
 		else
-			dataFile = Instance.new("Folder")
+			dataFile = Instance.new(data.__className or "Folder")
 		end
 		
 		for key, value in pairs(data) do
 			local attribute = GetAttributeName(key)
 			if attribute then
 				dataFile:SetAttribute(attribute, value)
-			elseif key ~= "__value" then
-				local entry = FolderEncoder.TableToFolder(value)
-				entry.Name = key
-				entry.Parent = dataFile
+			elseif not table.find(RESERVED_DATA_KEYS, key) then
+				FolderEncoder.TableToFolder(value, key, dataFile)
 			end
 		end
 	else
